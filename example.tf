20a62e1163abafc3bf4fbeb7441988de5f83f043
S E R A Y A <seraya.takahashi@outlook.com>
2018-10-08 13:52:09 +0200

    added example config file

---
+++ example.tf
@@ -0,0 +1,13 @@
+// A provider is responsible for creating and managing resources
+provider "aws" {
+  access_key = "AKIAIH6FYVH3BTXP37NQ"
+  secret_key = "ZOdpq/dzC7gOgjqovoMISMt5mKsumCtWhFoMehzt"
+  region     = "us-east-1"
+}
+
+// The resource block defines a resource that exists within the infrastructure.
+// It can be either physical (e.g. ec2 instance) or logical (e.g. heroku app)
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
